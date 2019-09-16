9442917f2cfe75cd2a0c6c2a44a9b4666b88ab25
halfwhole <limziwei2@gmail.com>
2019-09-16 23:52:32 +0800

    Refactor API

--- api.js
+++ api.js
@@ -18,6 +18,123 @@ function queryAPI(auth, path) {
     });
 }
 
+function queryModulesAPI(auth) {
+    /* EXAMPLE OF A MODULE
+    { id: '3f294850-b35f-452e-929c-95822999fc20',
+      createdDate: '2019-07-08T10:34:59.137+08:00',
+      creatorID: '5f0d8587-f144-4c23-91e1-9cef106e2ed8',
+      lastUpdatedDate: '2019-08-15T14:35:28.43+08:00',
+      lastUpdatedBy: '5f0d8587-f144-4c23-91e1-9cef106e2ed8',
+      name: 'CS2106',
+      startDate: '2019-07-08T10:34:00+08:00',
+      endDate: '2019-12-21T23:59:00+08:00',
+      publish: true,
+      parentID: '3f294850-b35f-452e-929c-95822999fc20',
+      resourceID: '00000000-0000-0000-0000-000000000000',
+      access:
+       { access_Full: false,
+         access_Read: true,
+         access_Create: false,
+         access_Update: false,
+         access_Delete: false,
+         access_Settings_Read: false,
+         access_Settings_Update: false },
+      courseName: 'Introduction to Operating Systems',
+      facultyCode: '39',
+      departmentCode: '252',
+      term: '1910',
+      acadCareer: 'UGRD',
+      courseSearchable: true,
+      allowAnonFeedback: 'N',
+      displayLibGuide: false,
+      copyFromID: '00000000-0000-0000-0000-000000000000',
+      l3: false,
+      enableLearningFlow: true,
+      usedNusCalendar: false,
+      isCorporateCourse: false,
+      creatorUserID: 'dcscrist',
+      creatorName: 'Cristina Carbunaru',
+      creatorEmail: 'dcscrist@nus.edu.sg',
+      termDetail:
+       { acadCareer: 'UGRD',
+         term: '1910',
+         description: '2019/2020 Semester 1',
+         termBeginDate: '2019-08-05T00:00:00+08:00',
+         termEndDate: '2019-12-07T00:00:00+08:00' },
+      isMandatory: false }
+    */
+
+    const MODULE_PATH = 'module/?populate=Creator%2CtermDetail%2CisMandatory';
+    return queryAPI(auth, MODULE_PATH);
+}
+
+function queryFoldersAPI(auth, parent_id) {
+    /* EXAMPLE OF A FOLDER
+    { access:
+        { access_Full: false,
+          access_Read: true,
+          access_Create: false,
+          access_Update: false,
+          access_Delete: false,
+          access_Settings_Read: false,
+          access_Settings_Update: false },
+      id: '9fce3f35-d6ab-4114-961c-bf141449f500',
+      createdDate: '2019-01-11T14:48:00+08:00',
+      creatorID: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
+      lastUpdatedDate: '2019-07-22T14:14:20.02+08:00',
+      lastUpdatedBy: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
+      name: 'Lecture Notes',
+      startDate: '2019-08-12T14:48:00+08:00',
+      endDate: '2019-12-12T14:48:00+08:00',
+      publish: true,
+      parentID: 'ffbd17d6-094a-4493-89c5-168f47b729b9',
+      rootID: 'ffbd17d6-094a-4493-89c5-168f47b729b9',
+      sortFilesBy: 'Date',
+      allowUpload: false,
+      uploadDisplayOption: 'Name',
+      viewAll: true,
+      folderScore: 0,
+      allowComments: true,
+      isTurnitinFolder: false,
+      totalFileCount: 26,
+      totalSize: 390119172,
+      subFolderCount: 22 } ]
+    */
+
+    const FOLDER_PATH = 'files/?populate=totalFileCount%2CsubFolderCount%2CTotalSize&ParentID=' + parent_id;
+    return queryAPI(auth, FOLDER_PATH);
+}
+
+function queryFilesAPI(auth, parent_id) {
+    /* EXAMPLE OF A FILE
+     { id: '7bc2e06e-24e0-4e59-968d-db8686b214a5',
+       parentID: '7bc2e06e-24e0-4e59-968d-db8686b214a5',
+       resourceID: '30f331f0-51c1-4261-ad3d-973119fb76b8',
+       publish: true,
+       name: 'Test 1 AY1920 Instructions.pdf',
+       allowDownload: true,
+       fileSize: 245018,
+       fileFormat: 'File',
+       fileName: 'Test 1 AY1920 Instructions.pdf',
+       creatorID: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
+       createdDate: '2019-09-09T18:58:58.817+08:00',
+       creatorName: 'Edmund Low',
+       creatorUserID: 'usplsye',
+       creatorEmail: 'usplsye@nus.edu.sg',
+       creatorMatricNo: '049907',
+       lastUpdatedDate: '2019-09-09T18:58:58.817+08:00',
+       lastUpdatedBy: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
+       lastUpdatedByName: 'Edmund Low',
+       lastUpdatedByUserID: 'usplsye',
+       lastUpdatedByEmail: 'usplsye@nus.edu.sg',
+       lastUpdatedByMatricNo: '049907',
+       comment: [] }
+    */
+
+    const FILES_PATH  = 'files/' + parent_id + '/file?populate=Creator%2ClastUpdatedUser%2Ccomment';
+    return queryAPI(auth, FILES_PATH);
+}
+
 function getDownloadURL(auth, path) {
     const options = {
         headers: { 'Authorization': auth },
@@ -61,4 +178,4 @@ async function downloadFolderAPI(auth, folder_id) {
     return await downloadAPI(auth, folder_path);
 }
 
-module.exports = { queryAPI, downloadFileAPI, downloadFolderAPI };
+module.exports = { queryModulesAPI, queryFoldersAPI, queryFilesAPI, downloadFileAPI, downloadFolderAPI };
--- exploreLuminusDirectory.js
+++ exploreLuminusDirectory.js
@@ -1,63 +1,15 @@
 const { Module, Folder, File } = require('./directory');
 const { readModuleMapping } = require('./moduleParser');
 const { readPrint } = require('./configParser');
-const { queryAPI } = require('./api');
+const { queryModulesAPI, queryFoldersAPI, queryFilesAPI } = require('./api');
 
 const PRINT = readPrint();
 const MAPPING = readModuleMapping();
 
-// TODO: Refactor queryAPI for modules, files, folders into api.js
-
 // Entry point for exploring all modules
 // Returns: an array of Modules
 async function exploreModules(auth) {
-    /* EXAMPLE OF A MODULE
-    { id: '3f294850-b35f-452e-929c-95822999fc20',
-      createdDate: '2019-07-08T10:34:59.137+08:00',
-      creatorID: '5f0d8587-f144-4c23-91e1-9cef106e2ed8',
-      lastUpdatedDate: '2019-08-15T14:35:28.43+08:00',
-      lastUpdatedBy: '5f0d8587-f144-4c23-91e1-9cef106e2ed8',
-      name: 'CS2106',
-      startDate: '2019-07-08T10:34:00+08:00',
-      endDate: '2019-12-21T23:59:00+08:00',
-      publish: true,
-      parentID: '3f294850-b35f-452e-929c-95822999fc20',
-      resourceID: '00000000-0000-0000-0000-000000000000',
-      access:
-       { access_Full: false,
-         access_Read: true,
-         access_Create: false,
-         access_Update: false,
-         access_Delete: false,
-         access_Settings_Read: false,
-         access_Settings_Update: false },
-      courseName: 'Introduction to Operating Systems',
-      facultyCode: '39',
-      departmentCode: '252',
-      term: '1910',
-      acadCareer: 'UGRD',
-      courseSearchable: true,
-      allowAnonFeedback: 'N',
-      displayLibGuide: false,
-      copyFromID: '00000000-0000-0000-0000-000000000000',
-      l3: false,
-      enableLearningFlow: true,
-      usedNusCalendar: false,
-      isCorporateCourse: false,
-      creatorUserID: 'dcscrist',
-      creatorName: 'Cristina Carbunaru',
-      creatorEmail: 'dcscrist@nus.edu.sg',
-      termDetail:
-       { acadCareer: 'UGRD',
-         term: '1910',
-         description: '2019/2020 Semester 1',
-         termBeginDate: '2019-08-05T00:00:00+08:00',
-         termEndDate: '2019-12-07T00:00:00+08:00' },
-      isMandatory: false }
-    */
-
-    const MODULE_PATH = 'module/?populate=Creator%2CtermDetail%2CisMandatory';
-    const modulesInfo = await queryAPI(auth, MODULE_PATH);
+    const modulesInfo = await queryModulesAPI(auth);
     // Filter out modules that haven't been mapped in MODULES.txt
     const filteredModulesInfo = modulesInfo.filter(moduleInfo => {
         return Object.keys(MAPPING).includes(moduleInfo['name']);
@@ -83,40 +35,7 @@ async function exploreModules(auth) {
 }
 
 async function exploreFolders(auth, parent_id) {
-    /* EXAMPLE OF A FOLDER
-    { access:
-        { access_Full: false,
-          access_Read: true,
-          access_Create: false,
-          access_Update: false,
-          access_Delete: false,
-          access_Settings_Read: false,
-          access_Settings_Update: false },
-      id: '9fce3f35-d6ab-4114-961c-bf141449f500',
-      createdDate: '2019-01-11T14:48:00+08:00',
-      creatorID: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
-      lastUpdatedDate: '2019-07-22T14:14:20.02+08:00',
-      lastUpdatedBy: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
-      name: 'Lecture Notes',
-      startDate: '2019-08-12T14:48:00+08:00',
-      endDate: '2019-12-12T14:48:00+08:00',
-      publish: true,
-      parentID: 'ffbd17d6-094a-4493-89c5-168f47b729b9',
-      rootID: 'ffbd17d6-094a-4493-89c5-168f47b729b9',
-      sortFilesBy: 'Date',
-      allowUpload: false,
-      uploadDisplayOption: 'Name',
-      viewAll: true,
-      folderScore: 0,
-      allowComments: true,
-      isTurnitinFolder: false,
-      totalFileCount: 26,
-      totalSize: 390119172,
-      subFolderCount: 22 } ]
-    */
-
-    const FOLDER_PATH = 'files/?populate=totalFileCount%2CsubFolderCount%2CTotalSize&ParentID=' + parent_id;
-    const foldersInfo = await queryAPI(auth, FOLDER_PATH);
+    const foldersInfo = await queryFoldersAPI(auth, parent_id);
     // Filter for folders that aren't submission folders, and are open folders
     const filteredFoldersInfo = foldersInfo.filter(folderInfo => {
         return !folderInfo['allowUpload'] && folderInfo['totalFileCount'] != null;
@@ -137,33 +56,7 @@ async function exploreFolders(auth, parent_id) {
 }
 
 async function exploreFiles(auth, parent_id) {
-    /* EXAMPLE OF A FILE
-     { id: '7bc2e06e-24e0-4e59-968d-db8686b214a5',
-       parentID: '7bc2e06e-24e0-4e59-968d-db8686b214a5',
-       resourceID: '30f331f0-51c1-4261-ad3d-973119fb76b8',
-       publish: true,
-       name: 'Test 1 AY1920 Instructions.pdf',
-       allowDownload: true,
-       fileSize: 245018,
-       fileFormat: 'File',
-       fileName: 'Test 1 AY1920 Instructions.pdf',
-       creatorID: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
-       createdDate: '2019-09-09T18:58:58.817+08:00',
-       creatorName: 'Edmund Low',
-       creatorUserID: 'usplsye',
-       creatorEmail: 'usplsye@nus.edu.sg',
-       creatorMatricNo: '049907',
-       lastUpdatedDate: '2019-09-09T18:58:58.817+08:00',
-       lastUpdatedBy: 'e98ffb04-eb4e-4f97-ab58-76ef3950b566',
-       lastUpdatedByName: 'Edmund Low',
-       lastUpdatedByUserID: 'usplsye',
-       lastUpdatedByEmail: 'usplsye@nus.edu.sg',
-       lastUpdatedByMatricNo: '049907',
-       comment: [] }
-    */
-
-    const FILES_PATH  = 'files/' + parent_id + '/file?populate=Creator%2ClastUpdatedUser%2Ccomment';
-    const filesInfo = await queryAPI(auth, FILES_PATH);
+    const filesInfo = await queryFilesAPI(auth, parent_id);
     const files = filesInfo.map(fileInfo => {
         return new File(fileInfo['id'], fileInfo['fileName'], fileInfo['lastUpdatedByName']);
     });
