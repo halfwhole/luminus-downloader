0090b4d2a954f8fb2431e227deae5f231213109c
halfwhole <limziwei2@gmail.com>
2020-02-28 11:26:20 +0800

    Authenticate with axios requests, RIP puppeteer

--- login.js
+++ login.js
@@ -1,52 +1,74 @@
-const puppeteer = require('puppeteer');
 const url = require('url');
+const qs = require('querystring');
+const axios = require('axios').create({
+    maxRedirects: 0,                          // intercept 302 redirects
+    validateStatus: (status) => status < 500, // also allow 302 redirects, not only 2xx
+});
 
 const { readUsername, readPassword, readPrint } = require('./configParser');
 
 const PRINT = readPrint();
 
+const VAFS_URL = 'https://vafs.nus.edu.sg/adfs/oauth2/authorize?' +
+                 'response_type=code&' +
+                 'client_id=E10493A3B1024F14BDC7D0D8B9F649E9-234390&' +
+                 'redirect_uri=https%3A%2F%2Fluminus.nus.edu.sg%2Fauth%2Fcallback&' +
+                 'resource=sg_edu_nus_oauth';
+const VAFS_PARAMS = {
+    AuthMethod: 'FormsAuthentication'
+};
+
+const ADFS_TOKEN_URL = 'https://luminus.nus.edu.sg/v2/api/login/adfstoken';
+const ADFS_TOKEN_PARAMS = {
+    grant_type: 'authorization_code',
+    client_id: 'E10493A3B1024F14BDC7D0D8B9F649E9-234390',
+    resource: 'sg_edu_nus_oauth',
+    redirect_uri: 'https://luminus.nus.edu.sg/auth/callback'
+};
+
+async function makeAxiosGet(url, cookies='') {
+    const res = await axios.get(url, { headers: { Cookie: cookies } });
+    return finishAxiosRequest(res);
+}
+
+async function makeAxiosPost(url, params, cookies='') {
+    const res = await axios.post(url, qs.stringify(params), { headers: { Cookie: cookies } });
+    return finishAxiosRequest(res);
+}
+
+function finishAxiosRequest(res) {
+    const setCookies = res.headers['set-cookie'];
+    const newCookies = (setCookies === undefined)
+        ? undefined
+        : setCookies.map(c => c.split('; ')[0]).join('; ');
+    const location = res.headers.location;
+    return [res, newCookies, location];
+}
+
 // Logs in the user, throwing an error if the login fails
-// Returns: a promise with the auth code
+// Returns: a promise with the access token
 async function login() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Move to LumiNUS
     if (PRINT) process.stdout.write('Logging into LumiNUS ... ');
-    await page.goto('https://luminus.nus.edu.sg/');
-
     const username = readUsername();
     const password = readPassword();
+    const login_params = { ...VAFS_PARAMS, UserName: username, Password: password };
 
-    await page.waitForSelector('.homepage');
-    await page.click('.btn-login');
-    await page.waitForSelector('#loginForm');
+    let res, cookies, location;
+    [res, cookies, location] = await makeAxiosPost(VAFS_URL, login_params);   // 302
+    if (res.status !== 302) throw 'Login invalid, please check your login credentials again.';
+    [res, cookies, location] = await makeAxiosGet(location, cookies=cookies); // 302
+    [res, _______, ________] = await makeAxiosGet(location, cookies=cookies); // 200
 
-    const userNameInput = await page.$('#userNameInput');
-    const passwordInput = await page.$('#passwordInput')
-    await userNameInput.type(username);
-    await passwordInput.type(password);
-    await passwordInput.press('Enter');
+    const queryString = url.parse(res.config.url).query;
+    const code = qs.parse(queryString).code;
+    const adfs_params = { ...ADFS_TOKEN_PARAMS, code };
 
-    await page.waitForNavigation();
-    const success = url.parse(page.url()).host === 'luminus.nus.edu.sg';
-    if (!success) {
-        throw 'Login invalid. Please check your login credentials again.'
-    }
+    [res, ________, ________] = await makeAxiosPost(ADFS_TOKEN_URL, adfs_params); // 200
 
-    // Listen to requests for API auth code
-    await page.setRequestInterception(true);
-    return new Promise((resolve, reject) => {
-        page.on('request', async (request) => {
-            if (request.url() === 'https://luminus.nus.edu.sg/v2/api/user/Profile?populate=photo,groupId,userRole') {
-                await browser.close();
-                if (PRINT) console.log('done!\n');
-                resolve(request.headers()['authorization']);
-            } else {
-                request.continue();
-            }
-        });
-    });
+    const access_token = res.data.access_token;
+    if (access_token === undefined) throw 'Login failed, please try again. LumiNUS sucks sometimes.';
+    if (PRINT) console.log('done!');
+    return 'Bearer ' + access_token;
 }
 
 module.exports = { login };
