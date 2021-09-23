8a898165da6855cbee24dba06ee14b676f8bb14f
Parth <67057645+parth-io@users.noreply.github.com>
2021-09-23 14:45:34 +0000

    Enable auto-generation of CONFIG.yaml Add new parameters --no-save and --reset-configuration Update README.md

--- README.md
+++ README.md
@@ -6,18 +6,7 @@ directory.
 
 ## Setting up
 
-There are two files you first have to set up:
-
-#### `CONFIG.txt`
-
-In the `config` directory, copy the file `CONFIG_EXAMPLE.txt` into a new file `CONFIG.txt`.
-
-Replace the following fields accordingly:
-* `username` NUSNET username
-* `password` NUSNET password
-* `directory_path` Path of your local directory for LumiNUS files, *relative to your home directory*
-
-#### `MODULES.txt`
+There is one file you first have to set up: `MODULES.txt`
 
 In the `config` directory, copy the file `MODULES_EXAMPLE.txt` into a new file `MODULES.txt`.
 
@@ -31,9 +20,17 @@ Ensure that you have 7-Zip available, through `sudo apt install p7zip-full` or `
 
 Run `npm install`, then run the main script using `node main.js`.
 
+Then, if you are a first-time user, enter the following details (the program will automatically save these details to `/config/CONFIG.yaml`)
+* `username` NUSNET username
+* `password` NUSNET password
+* `directory_path` Path of your local directory for LumiNUS files, *relative to your home directory*
+
+
 Options:
 - `--silent` or `-s`: No printing (apart from errors)
 - `--timeout` or `-t`: Timeout per request in ms, defaults to 5000
+- `--no-save` or `-n`: Will not save your credentials and preferred directory to a local file
+- `--reset-configuration` or `-r`: Reset credentials and preferred directory
 
 ![Example of running the script](./example.gif)
 
--- src/config.js
+++ src/config.js
@@ -1,42 +1,75 @@
 const fs = require('fs');
 const yargs = require('yargs');
+const prompt = require('prompt-sync')();
+const yaml = require('js-yaml');
+const homedir = require('os').homedir;
 
-const CONFIG_FILE  = 'config/CONFIG.txt';
+const CONFIG_FILE = 'config/CONFIG.yaml';
 const MODULES_FILE = 'config/MODULES.txt';
 const TIMEOUT = 5000;
+const CONFIG_FIELDS = ['username', 'password', 'directory_path'];
 
 const argv = yargs
-      .option('silent', {
-          alias: 's',
-          description: 'Runs the script silently',
-          type: 'boolean'
-      })
-      .option('timeout', {
-          alias: 't',
-          description: 'Timeout for each request in ms, defaults to ' + TIMEOUT,
-          type: 'integer'
-      })
-      .argv;
+    .option('silent', {
+        alias: 's',
+        description: 'Runs the script silently',
+        type: 'boolean'
+    })
+    .option('timeout', {
+        alias: 't',
+        description: 'Timeout for each request in ms, defaults to ' + TIMEOUT,
+        type: 'integer'
+    })
+    .option('no-save', {
+        alias: 'n',
+        description: 'Do not save the user configuration',
+        type: 'boolean'
+    })
+    .option('reset-configuration', {
+        alias: 'r',
+        description: 'Re-enter NUSNET credentials and directory path',
+        type: 'boolean'
+    })
+    .argv;
 
-function read(property) {
+const config = (() => {
+    const data = getFileData();
+    for (const [property, value] of Object.entries(data)) {
+        if (!value) {
+            if (property === 'directory_path') data[property] = prompt(property.toUpperCase() + ': ' + homedir);
+            else if (property === 'password') data[property] = prompt.hide(property.toUpperCase() + ': ');
+            else data[property] = prompt(property.toUpperCase() + ': ');
+        }
+    }
+    if (argv['no-save']) fs.unlink(CONFIG_FILE, (err) => console.log('Configuration was not saved to disk, you will have to re-enter your details again the next time'));
+    else fs.writeFileSync(CONFIG_FILE, yaml.dump(data), {encoding: 'utf8'});
+    return data;
+})()
+
+function createEmptyData() {
+    const data = {};
+    for (const key of CONFIG_FIELDS) data[key] = '';
+    return data;
+}
+
+function getFileData() {
+    if (argv['reset-configuration']) return createEmptyData();
     try {
-        const data = fs.readFileSync(CONFIG_FILE, 'utf8').toString();
-        const propertyLine = data.split('\n').filter(line => line.split('=')[0].trim() === property)[0];
-        const splitLine = propertyLine.split('=');
-        splitLine.shift();
-        const value = splitLine.join('=').trim();
-        return value;
-    } catch (e) {
-        throw 'Could not read ' + property + ' from ' + CONFIG_FILE + ', terminating.';
+        fs.accessSync(CONFIG_FILE);
+        return yaml.load(fs.readFileSync(CONFIG_FILE, {encoding: 'utf8'}));
+    }
+    catch (err) {
+        console.log('No existing configuration detected, creating new configuration')
+        return createEmptyData();
     }
 }
 
 function readUsername() {
-    return read('username');
+    return config.username;
 }
 
 function readPassword() {
-    return read('password');
+    return config.password;
 }
 
 function readPrint() {
@@ -48,21 +81,20 @@ function readTimeout() {
 }
 
 function readDirectoryPath() {
-    return read('directory_path');
+    return config.directory_path;
 }
 
 // Returns an object mapping LumiNUS module names to local module names
 function readModuleMapping() {
     try {
-        const data = fs.readFileSync(MODULES_FILE, 'utf8').toString();
+        const data = fs.readFileSync(MODULES_FILE, {encoding: 'utf8'}).toString();
         const lines = data.split('\n');
         let mapping = {};
         for (const line of lines) {
             if (line.trim() === '') continue;
             const splitLine = line.split(':');
             const luminusModuleName = splitLine[0].trim();
-            const localModuleName = splitLine[1] ? splitLine[1].trim() : null;
-            mapping[luminusModuleName] = localModuleName;
+            mapping[luminusModuleName] = splitLine[1] ? splitLine[1].trim() : null;
         }
         return mapping;
     } catch (e) {
